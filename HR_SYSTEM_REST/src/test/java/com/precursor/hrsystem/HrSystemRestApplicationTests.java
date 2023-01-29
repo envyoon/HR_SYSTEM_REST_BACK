319153e8573760b7ec8bb62d1869ada851998c6b
envyoon <krafftdj@daum.net>
2023-01-29 23:43:16 +0900

    feat: 초기상태 업로드

---
+++ HR_SYSTEM_REST/src/test/java/com/precursor/hrsystem/HrSystemRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.precursor.hrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSystemRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
