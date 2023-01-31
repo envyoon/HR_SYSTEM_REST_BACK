211a743ac68d9d558048dc016c65178c25acf694
envyoon <krafftdj@daum.net>
2023-01-31 20:18:04 +0900

    feat: junit update
    junit 사용기능 업데이트 하였음
    완벽하게 업데이트 한 것은 아님.
    
    사용시에는 라이브러리를 추가해서 사용을 하여야 한다.

--- HR_SYSTEM_REST/src/main/java/com/precursor/hrsystem/HrSystemRestApplication.java
+++ HR_SYSTEM_REST/src/main/java/com/precursor/hrsystem/HrSystemRestApplication.java
@@ -1,13 +1,37 @@
 package com.precursor.hrsystem;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
+
+
+//@RunWith(SpringRunner.class)
 @SpringBootApplication
 public class HrSystemRestApplication {
-
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrSystemRestApplication.class, args);
 	}
 
+	@Test
+	public void contextLoads() {
+	}
+	
+	@Test
+	public void testSqlSession() throws Exception{
+		System.out.println(sqlSession.toString());
+	}
+	
 }
