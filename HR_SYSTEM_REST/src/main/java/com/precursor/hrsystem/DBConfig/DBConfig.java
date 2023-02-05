2ac487d3febc09c39fc33bd2abf33d96546a14f9
envyoon <krafftdj@daum.net>
2023-02-05 23:15:11 +0900

    feat: DBConfig fix
    SqlSessionFactory 사용을 위해 DBConfig 수정을 진행하였지만..
    오류가 계속 나는 바람에
    
    application.properties 에 다시 mybatis 설정을 했다.
    
    그리고 Junit 기능 업데이트함

--- HR_SYSTEM_REST/src/main/java/com/precursor/hrsystem/DBConfig/DBConfig.java
+++ HR_SYSTEM_REST/src/main/java/com/precursor/hrsystem/DBConfig/DBConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
@@ -29,25 +30,29 @@ public class DBConfig {
 	
 	@Bean
 	public DataSource dataSource() throws Exception{
-		DataSource dataSource = new HikariDataSource(hikariConfig());
-		System.out.println(dataSource.toString());
-		return dataSource;
+		return new HikariDataSource(hikariConfig());
 	}
 	
 //	@Bean
-//	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
-//		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-//		sqlSessionFactoryBean.setDataSource(dataSource);
-//		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/sql-*.xml"));
-//		
-//		return sqlSessionFactoryBean.getObject();
-//	}
+//    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+//        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+//        sqlSessionFactoryBean.setDataSource(dataSource);
+//        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/static/mapper/**/*.xml"));
+//        sqlSessionFactoryBean.setConfiguration(mybatisConfig());
+//        return sqlSessionFactoryBean.getObject();
+//    }
+//
+//    @Bean
+//    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+//        return new SqlSessionTemplate(sqlSessionFactory);
+//    }
 //	
-//	@Bean
-//	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-//		return new SqlSessionTemplate(sqlSessionFactory);
-//	}
-	
+//    
+//    @Bean
+//    @ConfigurationProperties(prefix="mybatis.configuration")
+//    public org.apache.ibatis.session.Configuration mybatisConfig(){
+//    	return new org.apache.ibatis.session.Configuration();
+//    }
 }
 
 
