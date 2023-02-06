02ea33bef25d5c4d8f3e83e534455b86cacbc953
envyoon <krafftdj@daum.net>
2023-02-06 22:17:34 +0900

    feat: Sample MVC ADD

---
+++ HR_SYSTEM_REST/src/main/java/com/precursor/hrsystem/mvc/hrlist/a01_controller/SampleController.java
@@ -0,0 +1,8 @@
+package com.precursor.hrsystem.mvc.hrlist.a01_controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SampleController {
+	
+}
---
+++ HR_SYSTEM_REST/src/main/java/com/precursor/hrsystem/mvc/hrlist/a03_repository/SampleRepository.java
@@ -0,0 +1,8 @@
+package com.precursor.hrsystem.mvc.hrlist.a03_repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SampleRepository {
+
+}
