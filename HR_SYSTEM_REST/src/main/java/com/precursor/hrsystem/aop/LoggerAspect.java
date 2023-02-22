dbcd99f9e3e66ab40e39990ae7cf4e15d0f14c09
envyoon <krafftdj@daum.net>
2023-02-22 16:26:46 +0900

    feat: AOP 설정
    execution 사용하였음.
    within 주석처리 진행함.

---
+++ HR_SYSTEM_REST/src/main/java/com/precursor/hrsystem/aop/LoggerAspect.java
@@ -0,0 +1,33 @@
+package com.precursor.hrsystem.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Aspect
+@Slf4j
+public class LoggerAspect {
+	// within -> 이경로에 있는건 다 실행 하겠다. 
+	//@Around("within(com.precursor.hrsystem.*)")
+	@Around("execution(* com.precursor.hrsystem.a01_controller.*(..)) or execution(* com.precursor.hrsystem.a02_service.*Impl.*(..)) or execution(* com.precursor.hrsystem.a03_mapper.*Mapper.*(..))")
+	public Object logPrint(ProceedingJoinPoint joinPoint) throws Throwable {
+		String type = "";
+		String name = joinPoint.getSignature().getDeclaringTypeName();
+		if (name.indexOf("Controller") > -1) {
+			type = "Controller  \t:  ";
+		}
+		else if (name.indexOf("Service") > -1) {
+			type = "ServiceImpl  \t:  ";
+		}
+		else if (name.indexOf("Mapper") > -1) {
+			type = "Mapper  \t\t:  ";
+		}
+		log.debug(type + name + "." + joinPoint.getSignature().getName() + "()");
+		return joinPoint.proceed();
+	}
+
+}
